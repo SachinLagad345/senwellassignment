db0a1e16a79e5fed593b8eddc7e9cd03043989d9
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-16 17:43:00 +0530

    Added react assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment, useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const data = [];
+  
+    const [resdata, setResdata] = useState([]);
+    const [loading,setLoading] = useState(true);
+  
+    useEffect(() => {
+      fetch("https://6cw4vl6ty7.execute-api.ap-northeast-1.amazonaws.com/dev ")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data.body.data);
+          const mydata = data.body.data;
+          setResdata(prevOldData => {
+              return [prevOldData,...mydata];
+          });
+          setLoading(false);
+        })
+        .catch((e) => console.log(e));
+    }, []);
+  
+      resdata.map((item) => {
+        var oc = data.find((it) => it.name === item.CameraType);
+        if(oc !== undefined)
+        {
+          data[data.findIndex((it) => it.name === item.CameraType)].count +=1;
+        }
+        else if(item.CameraType !== undefined)
+        {
+          data.push({name : item.CameraType, count : 1});
+        }
+      })
+  
+      console.log(data);
+
+    return (
+      <Fragment>
+        <table>
+        {loading ? null : <thead>
+            <tr>
+              <th>CameraType</th>
+              <th>Count</th>
+            </tr>
+          </thead>}
+          <tbody>
+          {console.log("resdata type is " + typeof(resdata))}
+          {data.map((item,ind) => {
+            return (
+              <tr key={ind} className={item.count>0 && item.count<3?"red":(item.count<10 ? "yellow":"green")}>
+              <td>{item.name}</td>
+              <td>{item.count}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+      </Fragment>
+    );
 }
 
 export default App;
