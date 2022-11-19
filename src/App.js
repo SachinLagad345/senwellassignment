92cf1bee3de56fa6c56363e56f42eb2e952e8e54
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-19 11:49:31 +0530

    Initial commit for assignment

--- src/App.js
+++ src/App.js
@@ -1,54 +1,60 @@
 import './App.css';
 import { Fragment, useEffect, useState } from 'react';
+import Child from './components/Child';
 
 function App() {
-    const data = [];
+    const data = [{name:"sachin",age:26},{name:"sachin",age:26},{name:"sachin",age:26},
+    {name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},
+    {name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},
+    {name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},{name:"shrikant",age:26},
+    {name:"sagar",age:26},{name:"sagar",age:26},{name:"shashi",age:26},{name:"Ajinath",age:26},{name:"Krishna",age:5},
+    {name:"Krishna",age:5},{name:"Krishna",age:5},{name:"Krishna",age:5}];
   
-    const [resdata, setResdata] = useState([]);
-    const [loading,setLoading] = useState(true);
+    // const [resdata, setResdata] = useState([]);
+    const resdata = [];
   
-    useEffect(() => {
-      fetch("https://6cw4vl6ty7.execute-api.ap-northeast-1.amazonaws.com/dev ")
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data.body.data);
-          const mydata = data.body.data;
-          setResdata(prevOldData => {
-              return [prevOldData,...mydata];
-          });
-          setLoading(false);
-        })
-        .catch((e) => console.log(e));
-    }, []);
+    // useEffect(() => {
+    //   fetch("https://6cw4vl6ty7.execute-api.ap-northeast-1.amazonaws.com/dev ")
+    //     .then((res) => {
+    //       return res.json();
+    //     })
+    //     .then((data) => {
+    //       console.log(data.body.data);
+    //       const mydata = data.body.data;
+    //       setResdata(prevOldData => {
+    //           return [prevOldData,...mydata];
+    //       });
+    //       setLoading(false);
+    //     })
+    //     .catch((e) => console.log(e));
+    // }, []);
   
-      resdata.map((item) => {
-        var oc = data.find((it) => it.name === item.CameraType);
+      data.map((item) => {
+        var oc = resdata.find((it) => it.name === item.name);
         if(oc !== undefined)
         {
-          data[data.findIndex((it) => it.name === item.CameraType)].count +=1;
+          resdata[resdata.findIndex((it) => it.name === item.name)].count +=1;
         }
-        else if(item.CameraType !== undefined)
+        else
         {
-          data.push({name : item.CameraType, count : 1});
+          resdata.push({name : item.name, count : 1});
         }
       })
   
-      console.log(data);
+      console.log(resdata);
 
     return (
       <Fragment>
         <table>
-        {loading ? null : <thead>
+        <thead>
             <tr>
-              <th>CameraType</th>
+              <th>Name</th>
               <th>Count</th>
             </tr>
-          </thead>}
+          </thead>
           <tbody>
           {console.log("resdata type is " + typeof(resdata))}
-          {data.map((item,ind) => {
+          {resdata.map((item,ind) => {
             return (
               <tr key={ind} className={item.count>0 && item.count<3?"red":(item.count<10 ? "yellow":"green")}>
               <td>{item.name}</td>
@@ -58,6 +64,7 @@ function App() {
           })}
           </tbody>
         </table>
+        <Child></Child>
       </Fragment>
     );
 }
